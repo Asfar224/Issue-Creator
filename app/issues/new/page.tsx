c8cbfd1cfd3032b857692d99038ae27639af060b
Asfar <asfar.abid.chaudhay@gmail.com>
2024-07-15 00:53:09 +0500

    style

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -53,9 +53,10 @@ const page = () => {
      <>
         <div>
         <h1 className='mb-7 font-bold'>ENTER NEW ISSUE</h1>
-        {error && <Callout.Root color='red' className='mb-5'>
+        {error && <Callout.Root color='red' className='mb-5 max-w-lg '>
           <div className='flex flex-row '> 
-            <Callout.Text>{error}</Callout.Text>
+            <Callout.Text className='text-nowrap'>{error}</Callout.Text>
+            <div className='w-80'></div>
             <button onClick={handleCloseError}>
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-x-lg" viewBox="0 0 16 16">
                   <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z"/>
--- firebase-config.js
+++ firebase-config.js
@@ -5,13 +5,13 @@ import { getAuth} from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBtW5yQUtKbBJ87KEUAXECBC44Fal-WL6I",
-  authDomain: "issue-tracker-eeb9e.firebaseapp.com",
-  projectId: "issue-tracker-eeb9e",
-  storageBucket: "issue-tracker-eeb9e.appspot.com",
-  messagingSenderId: "937502124313",
-  appId: "1:937502124313:web:f6c7ec7c0d4078e36bfd88",
-  measurementId: "G-QSWS7BEEZV"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId:process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
